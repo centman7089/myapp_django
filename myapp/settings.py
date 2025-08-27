a92b8739c759c8a026cb6d71153782d48142b171
Centman7089 <innocentimitini@gmail.com>
2025-08-27 16:52:00 +0100

    initial commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["jang-api.onrender.com","localhost", "127.0.0.1"])
 
 
 
